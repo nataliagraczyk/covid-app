17b54069af86e6d46df247f5bbed1fbc5379fab7
nataliagraczyk <natalia.graczyk.97@gmail.com>
2022-01-12 21:55:17 +0000

    Add line plot

--- app.R
+++ app.R
@@ -9,6 +9,8 @@ library(curl)
 library(plyr)
 library(magrittr)
 library(lubridate)
+library(data.table)
+library(scales)
 
 # Get the data from an API
 
@@ -31,6 +33,7 @@ dates_only_df <- complete_df[startsWith(names(complete_df), "All.dates")]
 general_df <-complete_df[c(2,6,10)]
 stat_df <- complete_df[c(3:5)]
 
+
 convert_factors_to_numeric <- function(my_dataframe) { 
   as.data.frame(lapply(my_dataframe,
                        function(x) {
@@ -51,8 +54,14 @@ stat_converted <- convert_factors_to_numeric(stat_df)
 df <- cbind(general_df,stat_converted)
 df <- cbind(df, dates_converted)
 
+dates_df <- transpose(dates_converted)
 colnames(df) <- sub("All.", "", colnames(df))
 colnames(df) <- sub("dates.", "", colnames(df))
+
+colnames(dates_df) <- df$country
+rownames(dates_df) <- colnames(df[7:length(df)])
+
+dates <- as.Date(rownames(dates_df), format = "%Y.%m.%d")
 
 df <- df %>%
   add_column(new_cases = df[[7]]-df[[8]],
@@ -74,41 +83,33 @@ ui <- fluidPage(
                              c("New cases", "Recovered", "Deaths" )),
                  actionButton("cases_btn", "Show", class = "btn-warning")
                ),
-               mainPanel(
-                 tabsetPanel(
-                   tabPanel("Barchart",
-                            withSpinner(plotOutput("plot"))
-                   ),
-                   tabPanel("NA for the following countries",
-                            verbatimTextOutput("nulls"))
-                 )
+               mainPanel(withSpinner(plotOutput("plot")
+               )
                )
              )
     ),
-    tabPanel("Historical data", "Animated historical data for each continent"),
     tabPanel("Country comparison", 
              fluidRow(
                column(6,
                       selectInput("continent1", 
                                   "Select continent:", 
-                                  choices = unique(df$continent)),
+                                  choices = unique(df$continent),),
                       selectInput("country1", 
                                   "Select first country to compare:", 
-                                  choices = NULL),
-                      tableOutput("data1")
+                                  choices = NULL)
                ),
                column(6,
                       selectInput("continent2", 
                                   "Select continent:", 
-                                  choices = unique(df$continent)),
+                                  choices = unique(df$continent),
+                                  selected = "Europe"),
                       selectInput("country2", 
                                   "Select second country to compare:", 
-                                  choices = NULL),
-                      tableOutput("data2")
+                                  choices = NULL)
                )
              ),
              fluidRow(
-               withSpinner(plotOutput("plot_comparison"))
+               withSpinner(plotOutput("plot_comparison", width = 1500, height = 600))
              )
     )
   )
@@ -128,10 +129,21 @@ server <- function(input, output, session) {
            head(10)%>%
            ggplot(aes(x = reorder(country, new_cases),
                       y = new_cases)) +
-           geom_bar(stat = "identity", color = "black", fill = "darkturquoise") + 
+           geom_bar(stat = "identity", color = "black", fill = "darkturquoise") +
+           labs(
+             title = "New cases of Covid-19 yesterday",
+             subtitle = "The chart shows the TOP 10 countries with the highest number of cases",
+             caption = "Source: The data comes from the public API: https://mmediagroup.fr/covid-19"
+           ) +
            coord_flip() +
            xlab ("Country") +
-           ylab ("New Covid-19 cases"))
+           ylab ("New Covid-19 cases")) +
+      theme(
+        plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 13, face = "bold", hjust = 0.5),
+        plot.caption = element_text(size = 13, face = "italic", hjust = 0)
+      ) +
+      scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3))
   })
   
   output$nulls <- renderPrint({
@@ -160,21 +172,37 @@ server <- function(input, output, session) {
     updateSelectInput(inputId = "country2", choices = choices) 
   })
   
-  output$data1 <- renderTable({
-    req(input$country1)
-    continent1() %>% 
-      filter(country == input$country1) %>% 
-      select(population, new_cases)
-  })  
-  
-  output$data2 <- renderTable({
-    req(input$country2)
-    continent2() %>% 
-      filter(country == input$country2) %>% 
-      select(population, new_cases)
+  c1 <- reactive({
+    switch(input$country1, dates_df[,input$country1])
   })
   
-  # space for plot #
+  c2 <- reactive({
+    switch(input$country2, dates_df[,input$country2])
+  })
+  
+  output$plot_comparison <- renderPlot({
+    
+    ggplot(dates_df, aes(x=dates)) + 
+      geom_line(aes(y = c1(), color = input$country1), size = 1.5) + 
+      geom_line(aes(y = c2(), color = input$country2), size = 1.5) +
+      labs(
+        title = "Cumulative confirmed Covid-19 cases",
+        subtitle = "Data from the period: 22/01/2020 - now",
+        caption = "Source: The data comes from the public API: https://mmediagroup.fr/covid-19",
+        color = "Country"
+      ) +
+      xlab ("Date") +
+      ylab ("Confirmed Covid-19 cases") + 
+      scale_x_date(date_breaks = "4 months",
+                   date_labels = "%b %Y") +
+      theme(
+        axis.text.x=element_text(angle=60, hjust=1),
+        plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 13, face = "bold", hjust = 0.5),
+        plot.caption = element_text(size = 13, face = "italic", hjust = 0)
+      ) +
+      scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3))
+  })
 }
 
 shinyApp(ui, server)
